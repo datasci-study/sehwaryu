f368f91f50efcb28c5c4480c43ee393d16edcdea
sophryu99 <sehwa_ryu@berkeley.edu>
2020-09-04 23:14:57 +0900

    List2: 4837 진행중

--- SWEA/List2_4837_부분집합의합.py
+++ SWEA/List2_4837_부분집합의합.py
@@ -5,4 +5,31 @@
 # [출력]
 # 각 줄마다 "#T" (T는 테스트 케이스 번호)를 출력한 뒤, 답을 출력한다.
 
+# combination 라이브러리 사용
+from itertools import combinations
+
+lst = [1,2,3]
+
+# 함수 생성
+def createCombination(a):
+    sets = []
+    # 우선 리스트에 대한 부분집합 모두 생성
+    for i in range(0, len(a)+1):
+        c = combinations(a, i)
+        sets.extend(c)
+    # 해당 부분집합 안에 길이가 N인 부분집합만 새로운 리스트에 추가
+    set_length = []
+    for j in sets:
+        if len(j) == 3:
+            set_length.append(j)
+    sum(set_length)
+    
+createCombination(lst)
+#T = int(input())
+# for i in range(T):
+#     #lst = [1,2,3,4,5,6,7,8,9,10,11,12]
+#     result = []
+#     #N, K = map(int,input().split())
+#     print(result)
+
 
