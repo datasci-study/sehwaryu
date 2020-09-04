72ba0bf9eb1759df3fc5c14b5f0c0b804a95cd38
sophryu99 <sehwa_ryu@berkeley.edu>
2020-09-04 23:43:37 +0900

    List2: 4839 진행중

--- SWEA/List2_4839_이진탐색.py
+++ SWEA/List2_4839_이진탐색.py
@@ -5,10 +5,33 @@
 # [출력]
 # 각 줄마다 "#T" (T는 테스트 케이스 번호)를 출력한 뒤, A, B, 0 중 하나를 출력한다.
 
-T = int(input())
-for t in range(T):
-    P, A, B = map(int,input())
-    count_A = 0
-    count_B = 0
-    l = 1
-    c = int((l+P)/2)
+# 이진탐색 함수 만들기
+# p = 전체 쪽수, l = 첫째 쪽수 a = 찾아야하는 쪽수
+def binarySearch(p, l, a):
+    c = (l+p)/2
+    print(c)
+    # 이진탐색을 몇번 실시하는지 담는 변수
+    count = 0 
+    # a가 중간값보다 큰 경우 l = c
+    if a > c:
+        count +=1
+        binarySearch(p, c, a)
+    # a가 중간값보다 작은 경우 p = c
+    elif a < c:
+        count +=1
+        binarySearch(c,l,a)
+    elif c == a:
+        print(count)
+        
+binarySearch(400,1,300)
+
+
+
+
+# T = int(input())
+# for t in range(T):
+#     P, A, B = map(int,input())
+#     count_A = 0
+#     count_B = 0
+#     l = 1
+#     c = int((l+P)/2)
